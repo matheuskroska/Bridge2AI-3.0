209938b0c324766bb358bdea989a4a5b303f071d
Matheus <kroskamatheus@gmail.com>
2023-02-18 00:28:01 -0300

    add completion route

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
-const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
+const cors = require("cors");
+const { OpenAIApi, Configuration } = require("openai");
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+// Create an OpenAI API client with your API key
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const response = openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: "Corrija: Olá, meu neme é João.",
-  max_tokens: 50,
-  temperature: 0,
-}).then((response) => {
-    console.log(response.data.choices[0].text);
+
+// Enable CORS for cross-origin requests
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Define a route to handle requests to your API
+app.post("/completion", async (req, res) => {
+  const prompt = req.body.prompt;
+  try {
+    // Call the OpenAI API to generate text completion
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: req.query.max_tokens || 50,
+      temperature: req.query.temperature || 0,
+    });
+
+    // Return the generated text to the client
+    res.send(response.data.choices[0].text);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error generating text completion");
+  }
 });
 
+// Start the web server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
