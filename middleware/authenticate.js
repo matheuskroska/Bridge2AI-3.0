5da078c153e6e551483ac87b547583f7a9870944
Matheus <kroskamatheus@gmail.com>
2023-02-23 16:02:07 -0300

    hidding secret key into env variables

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,5 +1,7 @@
-const secret = "secret_key";
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const secret = process.env.SECRET_KEY;
+
 const authenticate = (req, res, next) => {
   const bearerHeader = req.headers["authorization"];
 
@@ -11,7 +13,6 @@ const authenticate = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(bearerToken, secret);
-    // Add the decoded token to the request object for use in the next middleware function or endpoint
     req.decoded = decoded;
     next();
   } catch (err) {
